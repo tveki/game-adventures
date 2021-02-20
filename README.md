058706708f768eafb4a05ee007110e9bef127327
Takács Viktor <tveki@webstar.hu>
2021-02-20 19:51:14 +0100

    Első java óra Terkával, Mátéval, Bandival

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # game-adventures
+
+we have adventures with logic games (the set game, logeo, etc)
---
+++ set-game/src/main/java/org/tveki/games/setgame/Ember.java
@@ -0,0 +1,29 @@
+package org.tveki.games.setgame;
+
+public class Ember {
+
+    private String name;
+
+    public Ember(String name) {
+        this.name = name;
+    }
+
+    public int eszik(String kaja, int kaloria) {
+        System.out.println(name + " ezt ette: " + kaja + ", ennyi kalória volt benne: " + kaloria);
+        final int kapottEletero = 10 * kaloria;
+        System.out.println("Kapott életerő: " + kapottEletero);
+        return kapottEletero;
+    }
+
+    public int fut(int tavolsag) {
+        final int elegetettKaloria = 2 * tavolsag;
+        System.out.println(name + " ennyi km-t futott: " + tavolsag + ", ennyi kalóriát égetett el: " + elegetettKaloria);
+        return elegetettKaloria;
+    }
+
+    public void alszik() {
+        System.out.println(name + " alszik.");
+        System.out.println("--------------nap vége--------------");
+    }
+
+}
